9b9e234b4e727233bf7e5e88fb07fbcc2abe9116
CHHAVI SHARMA <500053339@DDN.UPES.AC.IN>
2019-08-04 21:13:42 +0530

    'database-added'

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,18 @@
 var data = [{item:'wake up'},{item:'get ready'},{item:'catch the bus'}];
+var mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://chhavi:chhavi123%2A@todo-bkmvy.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true });
 
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+var itemOne = Todo({item:'get ready'}).save(function(err){
+  if(err) throw err;
+  console.log('item saved');
+
+});
 module.exports = function(app){
 
   app.get('/todo',function(req,res){
