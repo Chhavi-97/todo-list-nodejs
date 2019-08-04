623e2ca79e20c8e64da783c34e5079d005732f73
CHHAVI SHARMA <500053339@DDN.UPES.AC.IN>
2019-08-04 22:12:50 +0530

    'completed-todo'

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,4 @@
-var data = [{item:'wake up'},{item:'get ready'},{item:'catch the bus'}];
+// var data = [{item:'wake up'},{item:'get ready'},{item:'catch the bus'}];
 var mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://chhavi:chhavi123%2A@todo-bkmvy.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true });
 
@@ -8,34 +8,41 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo', todoSchema);
 
-var itemOne = Todo({item:'get ready'}).save(function(err){
-  if(err) throw err;
-  console.log('item saved');
-
-});
+// var itemOne = Todo({item:'get ready'}).save(function(err){
+//   if(err) throw err;
+//   console.log('item saved');
+//
+// });
 module.exports = function(app){
 
   app.get('/todo',function(req,res){
 
-   res.render('todo',{todos:data});
-
-  });
+   Todo.find({},function(err,data){
+     if(err)throw err;
+     res.render('todo',{todos:data});
+   });
+   });
 
   app.post('/todo',function(req,res){
-   data.push(req.body);
-   console.log(req.body);
 
-   res.json(data);
+    var newTodo = Todo(req.body).save(function(err,data){
+      if(err) throw err;
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item',function(req,res){
 
-   data = data.filter(function(todo){
 
-     return todo.item.replace(/ /g, '-')!== req.params.item;
-   });
-   console.log(req.params.item);
-   res.json(data);
-
+  Todo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+    if(err) throw err;
+    res.json(data);
   });
+   // data = data.filter(function(todo){
+   //
+   //   return todo.item.replace(/ /g, '-')!== req.params.item;
+   });
+   // console.log(req.params.item);
+   // res.json(data);
+
 };
